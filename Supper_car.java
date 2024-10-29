0e2c92292eafde3a212d7c29676e304d51e0d5f5
khoa Minh <khoakutip123@gmail.com>
2024-10-29 21:09:07 +0700

    them get set suppercar

--- Supper_car.java
+++ Supper_car.java
@@ -2,4 +2,10 @@
 public class Supper_car{
     private String CompanyCar;
     public static int QuantityCar = 0;
+    public String getCompanyCar(){
+        return CompanyCar;
+    }
+    public void setCompanyCar(String CompanyCar){
+        this.CompanyCar = CompanyCar;
+    }
 }
