dc1c3c0855608b8ee2ef15b82b429e28c73faffd
khoa Minh <khoakutip123@gmail.com>
2024-10-30 19:09:08 +0700

    them ham khoi tao

--- Supper_car.java
+++ Supper_car.java
@@ -10,6 +10,12 @@ public class Supper_car extends Car{
     public Supper_car(int id_car,int pricebuy,int pricesell,float weight,float length,String Companycar){
         super(id_car,pricebuy,pricesell,weight,length);
         this.CompanyCar = Companycar;
+        QuantityCar++;
+    }
+    public Supper_car(Supper_car sc){
+        super(sc.getId_car(),sc.getPricebuy(),sc.getPricesell(),sc.getWeight(),sc.getLength());
+        CompanyCar = sc.CompanyCar;
+        QuantityCar++;
     }
     public String getCompanyCar(){
         return CompanyCar;
