9e4fd6e4119d838071627d8df37ac6e579227b89
khoa Minh <khoakutip123@gmail.com>
2024-10-30 10:14:51 +0700

    them thuoc tinh get set

--- Car.java
+++ Car.java
@@ -1,7 +1,33 @@
+
+
+
 public class Car {
     private int pricesell;
     private int pricebuy;
     private float weight;
     private float length;
-    
+    public int getPricesell(){
+        return pricesell;
+    }
+    public int getPricebuy() {
+        return pricebuy;
+    }
+    public float getWeight() {
+        return weight;
+    }
+    public float getLength() {
+        return length;
+    }
+    public void setPricebuy(int pricebuy) {
+        this.pricebuy = pricebuy;
+    }
+    public void setPricesell(int pricesell) {
+        this.pricesell = pricesell;
+    }
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    public void setLength(float length) {
+        this.length = length;
+    }
 }
--- Supper_car.java
+++ Supper_car.java
@@ -1,7 +1,9 @@
-
 public class Supper_car extends Car{
     private String CompanyCar;
     public static int QuantityCar = 0;
+    public Supper_car(){
+        CompanyCar = "none";
+    }
     public String getCompanyCar(){
         return CompanyCar;
     }
