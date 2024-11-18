fcf64c0742de9b1d32b251d90340eff76ce002a9
lyhieunghia <nghiasd2020@gmail.com>
2024-11-18 12:35:01 +0700

    add search method for billmanager

--- customermain.java
+++ customermain.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import carstore.CustomerManager;
 import java.util.Scanner;
 public class customermain {
@@ -6,5 +7,18 @@ public class customermain {
         CustomerManager cusmanager = new CustomerManager();
         cusmanager.readFromFile();
         cusmanager.customerManagementMenu(new Scanner(System.in));
+=======
+import java.util.Scanner;
+
+import department_car.List_Car;
+public class customermain {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        List_Car car = new List_Car();
+        car.menuForCustomer(sc);
+
+>>>>>>> Stashed changes
     }
 }
